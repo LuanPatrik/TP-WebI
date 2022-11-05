f0cd2b038ab3d03e44607fa947845ddce79c6a21
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2022-11-05 10:22:11 -0300

    Atualização CSS

---
+++ Screens/Home/home.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/Home/home.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../../Assets/logo.png" alt="Logo">
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Eventos</li>
+        </ul>
+    </header>
+    <h1>Hello World!</h1>
+    <footer>
+        <div class="box">
+            <div class="redes">
+                <label for="">Redes Sociais</label>
+                <a href="http://www.instagram.com">Instagram</a>
+                <a href="">Facebook</a>
+                <a href="">Twitter</a>
+            </div>
+            <div class="contato">
+                <label for="">Contatos</label>
+                <label for="">E-mail</label>
+                <label for="">Telefone</label>
+            </div>
+        </div>
+        
+        <div class="privacidade">
+            <label for="Política de Privacidade"></label>
+            <label for="">© Copyright Próxima Parada - 2022 / Criado por Luan</label>
+        </div>
+    </footer>
+</body>
+</html>
+
